a7e53b253c21bcd19eab043a32dbe03ce1a7fd0e
MayankLaxkar <mayanklaxkar10@gmail.com>
2022-01-04 08:58:16 +0530

    2nd new commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import React from 'react',
+
+class index extends Component {
+    render()
+    return {
+        <div>
+
+        Hi I will create a new line and commit it
+        </div>
+
+    }
+}
